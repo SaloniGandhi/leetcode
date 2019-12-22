d706b51cbe28e50bcd940210e11103df6d7025f0
Saloni Gandhi <salonig97@gmail.com>
2019-12-22 12:36:01 -0500

    Update openlock-752.py

--- openlock-752.py
+++ openlock-752.py
@@ -4,33 +4,27 @@ class Solution:
         visited=[]
         q.append('0000')
         visited.append('0000')
+        level=0
         while(q):
-            level=0
             currentstate=q.pop(0)
             if currentstate==target:
                 return level
             if currentstate in deadends:
                 continue
-            elif currentstate not in deadends:
+            else :
                 #create the two possible strings of the current state
                 lock=currentstate
                 for i in range(0,4):
                     changeposition=lock[i]
-                    if changeposition=='9':
-                        changeposition='0'
-                    else:
-                        changepsoition1=changeposition-'0'+'1'
-                        changeposition2=changeposition-'0'-'1'
-                    s1=lock[0:i]+changeposition1+lock[i+1:]
-                    s2=lock[0:i]+changeposition2+lock[i+1:]             
+                    cp1=int(lock[i])+1 if int(lock[i])+1<10 else 0
+                    cp2=int(lock[i])-1 if int(lock[i])-1>=0 else 9
+                    s1=lock[0:i]+str(cp1)+lock[i+1:]
+                    s2=lock[0:i]+str(cp2)+lock[i+1:]             
                 if s1 not in visited and s1 not in deadends:
                     visited.append(s1)
                     q.append(s1)
                 if s2 not in visited and s2 not in deadends:
                     visited.append(s2)
                     q.append(s2)
-                
-                
-                
             level+=1
-        return level
+        return -1
