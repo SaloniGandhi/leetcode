78102377e47c1cc2aeb6c428dcb778abc1778a42
Saloni Gandhi <salonig97@gmail.com>
2020-03-26 17:52:21 -0400

    Update 160. Intersection of Two Linked Lists.py

--- 160. Intersection of Two Linked Lists.py
+++ 160. Intersection of Two Linked Lists.py
@@ -10,3 +10,25 @@
             headB = headB.next
         
         return None
+'''def findlen(thead):
+            count=0
+            while(thead):
+                count+=1
+                thead=thead.next
+            return count
+        
+        alen=findlen(headA)
+        blen=findlen(headB)
+        if blen>alen:
+            #blen is the longer list
+            #let alen always be the standard bigger list
+            headA,headB=headB,headA
+            
+        for i in range(abs(alen-blen)):
+            headA=headA.next
+        #brought it at the point where both these lists are now same
+        while(headA is not headB and headA and headB):
+            headB=headB.next
+            headA=headA.next
+        return headA
+        '''
